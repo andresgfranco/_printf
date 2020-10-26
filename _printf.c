9a5478edd5390b9912b298eccd8e65c21ce877a0
andresgfranco <andresgfranco0@gmail.com>
2020-10-26 01:29:58 +0000

    Update on subfunction from our printf

--- _printf.c
+++ _printf.c
@@ -13,7 +13,7 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-	printedCharacters = _formatevaluation(va_list valist, const char *format);
+	printedCharacters = format_reader(va_list valist, const char *format);
 
 	va_end(valist);
 	return (prc);
